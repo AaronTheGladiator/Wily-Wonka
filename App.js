e3dd8c37ef524a09a5c0ad52f862b4062e1014a8
Aaron Tomes <aarontomes16@gmail.com>
2020-11-02 20:59:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Book from './screens/book';
+import Search from './screens/search';
+
+export default class App extends React.Component {
+  render() {
+    return(
+      <AppContainer />
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Book: {screens: Book},
+  Search: {screens: Search}
+})
+
+const AppContainer = createAppContainer(TabNavigator);
